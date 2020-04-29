203cd06c65c9d3d8ddc2fd34d59ca19023213dc8
alexanderJPV <alexcyberay@gmail.com>
2020-04-29 16:52:05 -0400

    Rutas expres ejemply ejercicio

--- Ejem_express/README.md
+++ Ejem_express/README.md
@@ -1,1 +1,27 @@
-"# Pruebas_node" 
+# Node con expres
+_instalar el modulo express_
+## Comenzando
+
+https://expressjs.com/es/guide/routing.html
+
+ - npm install express --save
+
+ - node index.js
+
+### Pre-requisitos
+ - node v10.16.3
+ - Usar el programa postman para probar las rutas get,pos,put,delete
+# Ejercicio 🔧
+
+_Mediante la ruta GET_
+
+```
+Devuelve como respuesta el mensaje "esta cuarentena apesta"
+```
+
+_Y repite_
+
+```
+con todo los rutas
+```
+
--- Ejem_express/index.js
+++ Ejem_express/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const  app = express();
 
+// Ruta metodo GET deveulve la cadena "recivido..."
 app.get('/',(req,res) => {
     res.send('recivido...');
 });
+
+// Ruta metodo GET deveulve la cadena "guardado..."
 app.post('/',(req,res) => {
     res.send('guardado...');
 });
+
+// Ruta metodo GET deveulve la cadena "actualizando..."
 app.put('/',(req,res) => {
     res.send('actualizando...');
 });
+
+// Ruta metodo GET deveulve la cadena "eliminando..."
 app.delete('/',(req,res) => {
     res.send('eliminando...');
 });
 
+// servidor en el puerto 3000 => http://localhost:3000/
 app.listen(3000, () =>{
     console.log('server on port 3000');
 });
